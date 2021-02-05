1d7f41d2dcaee01b95f049db388d69556c82a90f
elf <49889930+elfq@users.noreply.github.com>
2021-02-05 03:04:55 -0500

    add anime-profile related commands

---
+++ cogs/profiles.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import requests
+import aiohttp
+
+
+class AnimeProfiles(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.command(
+    name="avatar",
+    help="Generate an anime avatar.",
+  )
+  async def avatar_(self, ctx):
+    async with aiohttp.ClientSession() as cs:
+      async with cs.get('https://shiro.gg/api/images/avatars') as r:
+        res = await r.json()
+        embed = discord.Embed(
+         description=f"**Here's your anime avatar!**",
+         color=discord.Colour.random()
+         )
+        embed.set_image(url=res['url'])
+        await ctx.reply(embed=embed)
+
+  @commands.command(
+    name="wallpaper",
+    help="Generate an anime wallpaper.",
+  )
+  async def wallpaper_(self, ctx):
+    async with aiohttp.ClientSession() as cs:
+      async with cs.get('https://shiro.gg/api/images/wallpapers') as r:
+        res = await r.json()
+        embed = discord.Embed(
+          description=f"**Here's your wallpaper!**",
+          color=discord.Colour.random()
+        )
+        embed.set_image(url=res['url'])
+        await ctx.reply(embed=embed)
+
+
+
+
+# More will be added soon! 
+
+def setup(bot):
+  bot.add_cog(AnimeProfiles(bot))
